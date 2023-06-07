522bd256efc359b0508ef7d574748331ecaf58e0
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-06-07 13:28:45 +0200

    condensed the first nested loop

--- other/trianglemesh/mySketch.js
+++ other/trianglemesh/mySketch.js
@@ -1,29 +1,29 @@
+// based on steve's makerspace tutorial on youtube
+
 let space;
 let points =[];
-let step = 7;
+let pos = [];
+let step = 14;
 
 function setup() {
-	createCanvas(400,400);
+	createCanvas(1080,1080);
 	background(220);
 	strokeWeight(4);
 	
 let space = width/step;
-for (let j = 0.5; j < step; j ++){
-	if (j % 2==0.5){
-		for (let i = 1; i <step; i ++){
-			let x = space * i;
-			let y = space * j;
-			point(x,y);
-			points.push (createVector(x,y));
+
+// make triangle point grid;
+for (let j = 1; j < step; j ++){
+	for (let i = 0.5; i <step-1; i ++){
+		let xoff = 0;
+		if (j % 2 == 1){
+		xoff = space/2;
 		}
-	} else if (j % 2==1.5){
-		for (let i = 0.5; i <step; i ++){
-			let x = space * i;
-			let y = space * j;
-			point(x,y);
-			points.push (createVector(x,y));
-		}
+		let x = space * i + xoff;
+		let y = space * j;
+		point(x,y)
+		points.push (createVector(x,y));
 	}
-	}
+}	
 }
 
