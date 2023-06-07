b4511f36450b1bd1bdb2cb3c279580fc83e95eef
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-06-07 18:06:20 +0200

    fighting to get the triangle grid working

--- other/trianglemesh/mySketch.js
+++ other/trianglemesh/mySketch.js
@@ -12,18 +12,29 @@ function setup() {
 	
 let space = width/step;
 
-// make triangle point grid;
 for (let j = 1; j < step; j ++){
-	for (let i = 0.5; i <step-1; i ++){
+	for (let i = 1; i <step; i ++){
 		let xoff = 0;
-		if (j % 2 == 1){
-		xoff = space/2;
-		}
+		//if (j % 2 == 1){
+		//xoff = space/2;
+		//}
 		let x = space * i + xoff;
 		let y = space * j;
-		point(x,y)
-		points.push (createVector(x,y));
+		point(x,y);
+		points.push({x, y});
+		let pos = points.length - 1;
+		 if(pos > step-1 && j % 2 == 0  && i >= 2){
+    		triangle(points[pos].x, points[pos].y, 
+								points[pos-(step-1)].x, points[pos-(step-1)].y,
+								points[pos-step].x, points[pos-step].y
+								)
+				triangle(points[pos].x, points[pos].y, 
+								points[pos-1].x, points[pos-1].y,
+								points[pos-step].x, points[pos-step].y
+								)
 	}
-}	
-}
 
+	}
+}		
+		
+}
