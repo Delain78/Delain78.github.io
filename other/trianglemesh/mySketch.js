13e3e32ba5812cb2f5f89e6d2bdb962a9d5b68f8
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-06-07 20:18:10 +0200

    got triangle grid to work :)

--- other/trianglemesh/mySketch.js
+++ other/trianglemesh/mySketch.js
@@ -4,24 +4,30 @@ let space;
 let points =[];
 let pos = [];
 let step = 14;
- 
+
 function setup() {
 	createCanvas(1080,1080);
-	background(220);
+	background(0);
+	colorMode(HSB);
 	strokeWeight(4);
+	
+
 	
 let space = width/step;
 
 for (let j = 1; j < step; j ++){
 	for (let i = 1; i <step; i ++){
 		let xoff = 0;
-		//if (j % 2 == 1){
-		//xoff = space/2;
-		//}
+		if (j % 2 == 1){
+		xoff = space/2;
+		}
 		let x = space * i + xoff;
 		let y = space * j;
-		point(x,y);
-		points.push({x, y});
+		
+		let h = 120 + y/9;
+		fill (h,100,100);
+		//point(x,y);
+		points.push(createVector(x,y));
 		let pos = points.length - 1;
 		 if(pos > step-1 && j % 2 == 0  && i >= 2){
     		triangle(points[pos].x, points[pos].y, 
@@ -30,6 +36,16 @@ for (let j = 1; j < step; j ++){
 								)
 				triangle(points[pos].x, points[pos].y, 
 								points[pos-1].x, points[pos-1].y,
+								points[pos-step].x, points[pos-step].y
+								)
+	}
+			if(pos > step-1 && j % 2 == 1 && i >= 2 ){
+    		triangle(points[pos].x, points[pos].y, 
+								points[pos-1].x, points[pos-1].y,
+								points[pos-(step-1)].x, points[pos-(step-1)].y
+								)
+				triangle(points[pos-1].x, points[pos-1].y, 
+							points[pos-(step-1)].x, points[pos-(step-1)].y,
 								points[pos-step].x, points[pos-step].y
 								)
 	}
