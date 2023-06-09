18d34571cae4f71ac496d7a914f1faa87c7749f6
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-06-09 17:23:18 +0200

    finally got the fractal function to work

--- lineart/hypnoticsquares/mySketch.js
+++ lineart/hypnoticsquares/mySketch.js
@@ -1,30 +1,45 @@
 // based on cubic disarray of William Kolomyjec
 
-let s = 400; 
+let sizeStart;
+let finalSize = 3;
+let totalSteps = 5;
 
 function setup() {
-	createCanvas(400, 400);
+	createCanvas(500, 500);
 	background(175);
 	colorMode(HSB);
+	 
+	sizeStart = 100;
 	
-for (let x = 0; x < width; x +=s ){
-	for (let y =0; y < height; y +=s){
-			drawRect(x,y);
-	}
-	}
+//for (let x = 0; x < width; x +=s ){
+	//for (let y =0; y < height; y +=s){
+			drawSquare(0,0,sizeStart, 1, 1, totalSteps);
+	//}
+	//}
 }
 
-function drawRect(x,y){
+function drawSquare(x,y,size, xMove, yMove, steps){
 	let h = 120 + y/9;
-	push();
-	translate (x,y);
 	stroke(h,100,100);
-	strokeWeight(4);
+	strokeWeight(3);
 	noFill();
-	rect(0,0,s,s);
-	pop();
-}
 
+  square(x, y, size);
+
+  if (steps >= 0 ) {
+    let newS = sizeStart * (steps / totalSteps) + finalSize;
+    let halfway = ((size - newS)) / 2;
+    let tempX = x + halfway;
+    let tempY = y + halfway;
+    let newX = tempX - ((x - tempX) / (steps + 2)) * xMove;
+    let newY = tempY - ((y - tempY) / (steps + 2)) * yMove;
+    drawSquare(newX, newY, newS, xMove, yMove, steps - 1);
+  }
+	
+	// example
+	//rect(((s *1/5)-20)/2,((s * 1/5)-20)/2,s * 4/5 + 20, s * 4/5 + 20);
+	
+}
 
 
 function keyPressed() {
