3aba6e7f0418c317c5b8a4254896c4ce94aaa437
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-06-11 15:08:43 +0200

    added splice, finally managed one fractal \o/

--- other/pietmondrianoop/mySketch.js
+++ other/pietmondrianoop/mySketch.js
@@ -2,42 +2,46 @@ let rectangles =[];
 
 function setup() {
   createCanvas(1080, 1080);
-	background(0);
+	background(100);
 	colorMode(HSB);
-	rectMode(CENTER);
 	
-
-    let x = width/2;
-    let y = height/2;
+		let x = 115;
+    let y = 115;
     let w = 850;
 		let h = 850;
+		let frame = 115
+		push()
+		translate (115,115)
 		let rectStart = new Rectangle(x,y,w,h)
-  rectangles.push(rectStart) ;
+ 		 rectangles.push(rectStart) 	
+		pop ()
+	
+	for (let i=rectangles.length-1; i >= 0; i--){
+		if (w == 850){
+			rectangles.splice(i,1)
+			let rectAX = new Rectangle (x,y,w/2,h)
+			let rectBX = new Rectangle (x + w/2,y,w/2,h)
+			rectangles.push(rectAX) 
+			rectangles.push(rectBX) 
+		}		
+		if (h == 850){
+			rectangles.splice(i,1)
+			let rectAY = new Rectangle (x,y,w,h/2)
+			let rectBY = new Rectangle (x,y + h/2,w,h/2)
+			rectangles.push(rectAY) 
+			rectangles.push(rectBY) 
+		}
+	}
+}
+
+	
+function draw(){
 	
 	for (let i=rectangles.length-1; i >= 0; i--){
 		rectangles[i].show();
 	}
 
-	//const { x, y } = coordinates;
-
-//for (var i = squares.length - 1; i >= 0; i--) {
-//const square = squares[i];
-
-//if (x && x > square.x && x < square.x + square.width) {
-//squares.splice(i, 1);
-//splitOnX(square, x);
-//}
-
-//if (y && y > square.y && y < square.y + square.height) {
-//squares.splice(i, 1);
-//splitOnY(square, y);
-//}
-//}
-	
-	
-	
 }
-
 
 function keyPressed() {
   if (key === 's') {
