a4ba7269c3e2f278d652ce6479358f57386c2403
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-06-11 15:04:07 +0200

    simplified the class

--- other/pietmondrianoop/rectangle.js
+++ other/pietmondrianoop/rectangle.js
@@ -12,24 +12,6 @@ class Rectangle {
     noFill();
    	rect(this.x, this.y, this.w, this.h);
   }
-
-splitSquaresWith(coordinates) {
-	 // Loops through the squares, and find if
-  // one should be split
-}
-	
-splitOnX(square, splitAt) {
-// Create two new squares, based on
-// splitting the given one at the
-// x coordinate given
-}
-
-splitOnY(square, splitAt) {
-// Create two new squares, based on
-// splitting the given one at the
-// y coordinate given
-}
-	
 	
 }
 
