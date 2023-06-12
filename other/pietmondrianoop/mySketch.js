1e48e4b7047443938ad6011ad85e842896103c37
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-06-12 12:22:46 +0200

    add newX, newY, newW, newH variable

--- other/pietmondrianoop/mySketch.js
+++ other/pietmondrianoop/mySketch.js
@@ -1,5 +1,5 @@
 let rectangles =[];
-let step = 6;
+let step = 3;
 
 function setup() {
   createCanvas(1080, 1080);
@@ -17,23 +17,51 @@ function setup() {
  		 rectangles.push(rectStart) 	
 		pop ()
 
-	//split on w/6
-	for (let i=rectangles.length-1; i >= 0; i--){
-			rectangles.splice(i,1)
-			for (let j = 1; j < step; j ++){
-			//split vertical
-				let rectAX = new Rectangle (x,y,w * j/step,h)
-				let rectBX = new Rectangle (x + w * j/step,y,w * (step-j)/step,h)
-				rectangles.push(rectAX) 
-				rectangles.push(rectBX) 
+
 	
-			// split horizontal
-				let rectAY = new Rectangle (x,y,w,h * j/step)
-				let rectBY = new Rectangle (x,y + h * j/step,w,h * (step - j)/step)
+//for (var i = 0; i < size; i += step) {
+  //splitSquaresWith({ y: i });
+  //splitSquaresWith({ x: i });
+//}
+
+	
+//split vertical
+for (let i=rectangles.length-1; i >= 0; i--){ //haak 1
+		 if (rectangles[i].w >= w * 2/3 ){ //haak 2 
+			let newX = rectangles[i].x ;
+			let newY = rectangles[i].y;
+			let newW = rectangles[i].w;
+			let newH = rectangles[i].h;
+			 
+			rectangles.splice(i,1)
+		
+			for (let j = 1; j < step; j ++){ //haak 3
+				let rectAX = new Rectangle (newX, newY, newW * j/step, newH)
+				let rectBX = new Rectangle (newX + newW * j/step, newY, newW * (step-j)/step, newH)
+				rectangles.push(rectAX) 
+				rectangles.push(rectBX)
+			}	 //haak 3
+		 } // haak 2
+ } // haak 1	
+
+// split horizontal
+	for (let i=rectangles.length-1; i >= 0; i--){ //haak 1 b
+		 if (rectangles[i].h <= h){ // haak 2 b
+			 let newX = rectangles[i].x ;
+			 let newY = rectangles[i].y;
+				let newW = rectangles[i].w;
+				let newH = rectangles[i].h;
+			 
+			rectangles.splice(i,1)
+			for (let j = 1; j < step; j ++){ //haak 3b
+				let rectAY = new Rectangle (newX, newY, newW, newH * j/step)
+				let rectBY = new Rectangle (newX, newY + newH * j/step, newW, newH * (step - j)/step)
 				rectangles.push(rectAY) 
 				rectangles.push(rectBY) 
-			}
-	}
+			} // haak 3b
+		} // haak 2b
+	}	// haak 1 b
+
 }
 
 	
@@ -50,4 +78,3 @@ function keyPressed() {
     save('vicsekfractal.png'); 
   }
 }
-
