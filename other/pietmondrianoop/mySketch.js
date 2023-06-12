2db30fcbb6b1087ab7853abc589107a9d8ffd993
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-06-12 15:49:11 +0200

    Update mySketch.js

--- other/pietmondrianoop/mySketch.js
+++ other/pietmondrianoop/mySketch.js
@@ -1,85 +1,69 @@
-let rectangles =[];
-let step = 6;
-let randomChance = 0.5;
+let rectangles = [];
+let step = 3;
 
 function setup() {
   createCanvas(1080, 1080);
-	background(100);
-	colorMode(HSB);
-	
-		let x = 115;
-    let y = 115;
-    let w = 850;
-		let h = 850;
-		let frame = 115
-		push()
-		translate (115,115)
-		let rectStart = new Rectangle(x,y,w,h)
- 		 rectangles.push(rectStart) 	
-		pop ()
+  background(100);
+  colorMode(HSB);
 
+  let x = 115;
+  let y = 115;
+  let w = 850;
+  let h = 850;
+  push();
+  translate(115, 115);
+  let rectStart = new Rectangle(x, y, w, h);
+  rectangles.push(rectStart);
+  pop();
 
-	
-//for (var i = 0; i < size; i += step) {
-  //splitSquaresWith({ y: i });
-  //splitSquaresWith({ x: i });
-//}
+  //split vertical
+  for (let i = rectangles.length - 1; i >= 0; i--) { //haak 1
+    let newX = rectangles[i].x;
+    let newY = rectangles[i].y;
+    let newW = rectangles[i].w;
+    let newH = rectangles[i].h;
 
-	
-//split vertical
-for (let i=rectangles.length-1; i >= 0; i--){ //haak 1
-		 if (rectangles[i].w >= w ){ //haak 2 
-			let newX = rectangles[i].x ;
-			let newY = rectangles[i].y;
-			let newW = rectangles[i].w;
-			let newH = rectangles[i].h;
-			 
-			rectangles.splice(i,1)
+    rectangles.splice(i, 1);
+
+    for (let j = 1; j < step; j++) { //haak 2
+      let rectAX = new Rectangle(newX, newY, (newW * j) / step, newH);
+      let rectBX = new Rectangle(newX + (newW * j) / step, newY, (newW * (step - j)) / step,newH);
+      rectangles.push(rectAX);
+      rectangles.push(rectBX);
+    } //haak 2
+  } // haak 1 
+
+  //split horizontal
+ for (let i = rectangles.length - 1; i >= 1; i--) {//haak 1b
+    let newX = rectangles[i].x;
+    let newY = rectangles[i].y;
+    let newW = rectangles[i].w;
+    let newH = rectangles[i].h;
 		
-			for (let j = 1; j < step; j ++){ //haak 3
-				let rectAX = new Rectangle (newX, newY, newW * j/step, newH)
-				let rectBX = new Rectangle (newX + newW * j/step, newY, newW * (step-j)/step, newH)
-				if (random() > 0.5){ // haak 4
-				rectangles.push(rectAX) 
-				rectangles.push(rectBX)
-				} // haak 4
-			}	 //haak 3
-		 } // haak 2
- } // haak 1	
-
-// split horizontal
-	for (let i=rectangles.length-1; i >= 0; i--){ //haak 1 b
-		 if (rectangles[i].h <= h){ // haak 2 b
-			 let newX = rectangles[i].x ;
-			 let newY = rectangles[i].y;
-				let newW = rectangles[i].w;
-				let newH = rectangles[i].h;
-			 
-			rectangles.splice(i,1)
-			for (let j = 1; j < step; j ++){ //haak 3b
-				let rectAY = new Rectangle (newX, newY, newW, newH * j/step)
-				let rectBY = new Rectangle (newX, newY + newH * j/step, newW, newH * (step - j)/step)
-				if (random() > 0.5){ // haak 4 b
-				rectangles.push(rectAY)  
-				rectangles.push(rectBY) 
-				} // haak 4 b
-			} // haak 3b
-		} // haak 2b
-	}	// haak 1 b
-
+	 if (newH == 850){ // haak 2b
+		 rectangles.splice(i, 1);
+		 
+		for (let j = 1; j < step; j++) { //haak 3b
+     // split horizontal
+      let rectAY = new Rectangle(newX, newY, newW, (newH * j) / step);
+      let rectBY = new Rectangle(newX, newY + (newH * j) / step,newW,(newH * (step - j)) / step);
+			 {
+      rectangles.push(rectAY);
+      rectangles.push(rectBY);
+			}
+   } //haak 3b
+	 } // haak 2b
+  } // haak 1b
 }
 
-	
-function draw(){
-	
-	for (let i=rectangles.length-1; i >= 0; i--){
-		rectangles[i].show();
-	}
-
+function draw() {
+  for (let i = rectangles.length - 1; i >= 0; i--) {
+    rectangles[i].show();
+  }
 }
 
 function keyPressed() {
-  if (key === 's') {
-    save('mondrian.png'); 
+  if (key === "s") {
+    save("mondrian.png");
   }
 }
