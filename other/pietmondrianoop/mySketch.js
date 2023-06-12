6140449bbdf5194f07af3293c8895b8b865167ef
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-06-12 12:53:04 +0200

    change of square being split
    Almost there. Now for getting it to for every square and not all the same

--- other/pietmondrianoop/mySketch.js
+++ other/pietmondrianoop/mySketch.js
@@ -1,5 +1,6 @@
 let rectangles =[];
-let step = 3;
+let step = 6;
+let randomChance = 0.5;
 
 function setup() {
   createCanvas(1080, 1080);
@@ -27,7 +28,7 @@ function setup() {
 	
 //split vertical
 for (let i=rectangles.length-1; i >= 0; i--){ //haak 1
-		 if (rectangles[i].w >= w * 2/3 ){ //haak 2 
+		 if (rectangles[i].w >= w ){ //haak 2 
 			let newX = rectangles[i].x ;
 			let newY = rectangles[i].y;
 			let newW = rectangles[i].w;
@@ -38,8 +39,10 @@ for (let i=rectangles.length-1; i >= 0; i--){ //haak 1
 			for (let j = 1; j < step; j ++){ //haak 3
 				let rectAX = new Rectangle (newX, newY, newW * j/step, newH)
 				let rectBX = new Rectangle (newX + newW * j/step, newY, newW * (step-j)/step, newH)
+				if (random() > 0.5){ // haak 4
 				rectangles.push(rectAX) 
 				rectangles.push(rectBX)
+				} // haak 4
 			}	 //haak 3
 		 } // haak 2
  } // haak 1	
@@ -56,8 +59,10 @@ for (let i=rectangles.length-1; i >= 0; i--){ //haak 1
 			for (let j = 1; j < step; j ++){ //haak 3b
 				let rectAY = new Rectangle (newX, newY, newW, newH * j/step)
 				let rectBY = new Rectangle (newX, newY + newH * j/step, newW, newH * (step - j)/step)
-				rectangles.push(rectAY) 
+				if (random() > 0.5){ // haak 4 b
+				rectangles.push(rectAY)  
 				rectangles.push(rectBY) 
+				} // haak 4 b
 			} // haak 3b
 		} // haak 2b
 	}	// haak 1 b
@@ -75,6 +80,6 @@ function draw(){
 
 function keyPressed() {
   if (key === 's') {
-    save('vicsekfractal.png'); 
+    save('mondrian.png'); 
   }
 }
