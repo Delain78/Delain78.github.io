f7a78cee779380f8c6891561c5f8d21688312a44
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-07-03 14:01:52 +0200

    uploaded cc 136.1 polar perlin noise loop

---
+++ codingtrain/codingChallenge/cc_136.1_polar_perlin_noise_loop/sketch.js
@@ -0,0 +1,34 @@
+let slider;
+let phase = 0;
+let zoff = 0;
+
+function setup() {
+  createCanvas(600, 600);
+  slider = createSlider(0, 10, 0, 0.1)
+}
+
+function draw() {
+  //noiseSeed(99);
+  background(0);
+  translate(width/2, height/2);
+  stroke (255);
+  noFill();
+ 
+  
+  beginShape();
+  noiseMax = slider.value();
+  for (let a = 0; a < TWO_PI; a +=0.1){
+    let xoff = map(cos(a), -1, 1, 0, noiseMax);
+    let yoff = map(sin(a), -1, 1, 0, noiseMax);
+    let r = map (noise(xoff, yoff,zoff), 0, 1, 100, 200);
+    let x = r * cos (a);
+    let y = r * sin (a);
+    vertex (x,y);
+  }
+  endShape(CLOSE)
+  
+  zoff += 0.01;
+  
+  //phase += 0.05;
+  //noLoop();
+}
