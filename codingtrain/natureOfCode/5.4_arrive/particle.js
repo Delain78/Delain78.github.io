8c3c0a19b53a0e4ff69a4d79b8fbf46bb507b3ea
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-07-06 16:23:21 +0200

    uploaded 5.4 arrive

---
+++ codingtrain/natureOfCode/5.4_arrive/particle.js
@@ -0,0 +1,87 @@
+class Vehicle {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.maxSpeed = 6;
+    this.maxForce = 0.4;
+    this.r = 16;
+  }
+
+  evade(vehicle) {
+    let pursuit = this.pursue(vehicle);
+    pursuit.mult(-1);
+    return pursuit;
+  }
+
+  pursue(vehicle) {
+    let target = vehicle.pos.copy();
+    let prediction = vehicle.vel.copy();
+    prediction.mult(10);
+    target.add(prediction);
+    fill(0, 255, 0);
+    circle(target.x, target.y, 16);
+    return this.seek(target);
+  }
+
+  arrive() {
+    //2nd argument true enables the arrival behaviour
+    return this.seek(target, true);
+  }
+
+  flee(target) {
+    return this.seek(target).mult(-1);
+  }
+
+  seek(target, arrival = false) {
+    let force = p5.Vector.sub(target, this.pos);
+
+    let diseredSpeed = this.maxSpeed;
+    if (arrival) {
+      let slowRadius = 100;
+      let distance = force.mag();
+      if (distance < slowRadius) {
+        diseredSpeed = map(distance, 0, slowRadius, 0, this.maxSpeed);
+      }
+    }
+    force.setMag(diseredSpeed);
+    force.sub(this.vel);
+    force.limit(this.maxForce);
+    return force;
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  edges() {
+    if (this.pos.x > width + this.r) {
+      this.pos.x = -this.r;
+    } else if (this.pos.x < -this.r) {
+      this.pos.x = width + this.r;
+    }
+    if (this.pos.y > height + this.r) {
+      this.pos.y = -this.r;
+    } else if (this.pos.y < -this.r) {
+      this.pos.y = height + this.r;
+    }
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxSpeed);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(2);
+    fill(255);
+    push();
+    translate(this.pos.x, this.pos.y);
+    rotate(this.vel.heading());
+    triangle(-this.r, -this.r / 2, -this.r, this.r / 2, this.r, 0);
+    pop();
+  }
+}
---
+++ codingtrain/natureOfCode/5.4_arrive/sketch.js
@@ -0,0 +1,23 @@
+//http://www.red3d.com/cwr/steer/
+
+let vehicle;
+let target; 
+
+function setup() {
+  createCanvas(400, 400);
+  vehicle = new Vehicle (200,100);
+}
+
+function draw() {
+  background(0);
+  
+  target = createVector (mouseX, mouseY)
+  fill (255,0,0);
+  noStroke();
+  circle(target.x, target.y, 32);
+  let steering = vehicle.arrive(target);
+  vehicle.applyForce(steering);
+  
+  vehicle.update();
+  vehicle.show();
+}
