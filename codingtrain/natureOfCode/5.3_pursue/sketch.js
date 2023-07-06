6729c9da978a50b2ee7538ee78bb257c9bb5051e
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-07-06 13:50:28 +0200

    uploaded 5.3 pursue

---
+++ codingtrain/natureOfCode/5.3_pursue/sketch.js
@@ -0,0 +1,24 @@
+//http://www.red3d.com/cwr/steer/
+
+let pursuer;
+let target; 
+
+function setup() {
+  createCanvas(400, 400);
+  pursuer = new Vehicle (100,100);
+  target = new Target (200,100);
+}
+
+function draw() {
+  background(0);
+  
+  let steering = pursuer.pursue(target);
+  pursuer.applyForce(steering);
+  
+  pursuer.update();
+  pursuer.show();
+  
+  target.edges();
+  target.update();
+  target.show();
+}
