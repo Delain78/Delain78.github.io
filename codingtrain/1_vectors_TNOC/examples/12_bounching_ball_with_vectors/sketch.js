415cdb5f0ade5b2b07aacd7816e2ae633debb0e8
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-07-13 21:33:48 +0200

    uploaded 1.2 bouncing ball with vecors

---
+++ codingtrain/1_vectors_TNOC/examples/12_bounching_ball_with_vectors/sketch.js
@@ -0,0 +1,26 @@
+let pos; // position
+let velocity;
+
+function setup() {
+  createCanvas(640, 240);
+
+  pos = createVector(100, 100);
+  velocity = createVector(1, 3.3);
+}
+
+function draw() {
+  background(255);
+  pos.add(velocity);
+
+  if (pos.x > width - 24 || pos.x < 24) {
+    velocity.x = velocity.x * -1;
+  }
+
+  if (pos.y > height - 24 || pos.y < 24) {
+    velocity.y  = velocity.y  * -1;
+  }
+
+  stroke(0);
+  fill(175);
+  circle(pos.x, pos.y, 48);
+}
