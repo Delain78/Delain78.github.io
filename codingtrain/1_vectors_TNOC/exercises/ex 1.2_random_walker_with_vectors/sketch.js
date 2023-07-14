3255a83d6c2d5134245d1b24a08f93b113d9124d
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-07-14 13:08:08 +0200

    uploaded ex 1.2 random walker with vectors

---
+++ codingtrain/1_vectors_TNOC/exercises/ex 1.2_random_walker_with_vectors/sketch.js
@@ -0,0 +1,32 @@
+let pos;
+
+function setup() {
+  createCanvas(400, 400);
+  background(51);
+  pos = createVector(200,200);
+  console.log(pos);
+}
+
+function draw() {
+  
+  stroke(255);
+  strokeWeight(2);
+  point(pos.x,pos.y);
+  
+  let r = floor(random(4));
+  
+  switch(r) {  // switch statement
+      case(0):
+        pos.x  = pos.x +1; //right
+        break;
+      case(1):
+        pos.x = pos.x - 1; //left
+        break;
+      case(2):
+        pos.y = pos.y + 1; //down
+        break;
+      case(3):
+        pos.y = pos.y - 1; //up
+        break;
+  }
+}
