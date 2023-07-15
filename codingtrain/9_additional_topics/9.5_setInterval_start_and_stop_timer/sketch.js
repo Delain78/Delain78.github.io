7bd2a77b0bcbac2c790c5d2edf10280005cefca2
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-07-15 16:57:50 +0200

    uploaded 9.5 setInterval start and stop timer

---
+++ codingtrain/9_additional_topics/9.5_setInterval_start_and_stop_timer/sketch.js
@@ -0,0 +1,39 @@
+let x = 0;
+
+let timer;
+let counter = 0;
+let interval = false;
+let button;
+
+function setup() {
+  createCanvas(200, 200);
+  timer = createP("timer");
+  button = createButton("start timer");
+  button.mousePressed(doTimer);
+}
+
+function doTimer() {
+  if (!interval) {
+    interval = setInterval(timeIt, 500);
+    button.html("stop timer");
+  } else {
+    clearInterval(interval);
+    interval = false;
+    button.html("start timer");
+  }
+}
+
+function timeIt() {
+  timer.html(counter);
+  counter++;
+}
+
+function draw() {
+  background(51);
+  stroke(255);
+  line(x, 0, x, height);
+  x = x + 3;
+  if (x > width) {
+    x = 0;
+  }
+}
