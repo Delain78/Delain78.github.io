e266f2755cfef2551b8ac1ddcab7243ee27149da
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-07-23 21:04:23 +0200

    uploaded 1.7 3D noise using open simplex noise

---
+++ codingtrain/13_perlinNoise/1.7_3D_Noise_open_simplex_noise/sketch.js
@@ -0,0 +1,38 @@
+// Based on Josh Forisha's implementation of opensimplex noise
+// https://github.com/joshforisha/open-simplex-noise-js
+// noise2D(x, y)
+// noise3D(x, y, z)
+// noise4D(x, y, z, w)
+
+let inc = 0.05;
+let zoff = 0;
+let zinc = 0.02;
+
+function setup() {
+  createCanvas(640, 360);
+  osnoise = new OpenSimplexNoise();
+}
+
+function draw() {
+  let yoff = 0;
+
+  loadPixels();
+  for (let y = 0; y < height; y++) {
+    let xoff = 0;
+    for (let x = 0; x < width; x++) {
+      let index = (x + y * width) * 4;
+      let r = osnoise.noise3D (xoff,yoff,zoff) * 255;
+      pixels[index + 0] = r; // red value
+      pixels[index + 1] = r; // green value
+      pixels[index + 2] = 200; // blue value
+      pixels[index + 3] = 255; // alpha value
+
+      xoff += inc;
+    }
+    yoff +=inc;
+  }
+  updatePixels();
+  
+  zoff +=zinc;
+  //noLoop();
+}
