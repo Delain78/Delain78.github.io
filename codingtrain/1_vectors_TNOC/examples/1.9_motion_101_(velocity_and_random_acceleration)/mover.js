707e51e5cd4a60a4bf0b61752c95da709c8f6a02
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-07-29 22:09:41 +0200

    uploaded 1.9 motion 101 (velocity and random acc)

---
+++ codingtrain/1_vectors_TNOC/examples/1.9_motion_101_(velocity_and_random_acceleration)/mover.js
@@ -0,0 +1,45 @@
+class Mover {
+  constructor() {
+    this.pos = createVector(width / 2, height / 2);
+    this.vel = createVector(0, 0);
+
+    this.topSpeed = 10;
+  }
+
+  update() {
+    this.acc = p5.Vector.random2D();
+    this.acc.mult(random(2));
+
+    this.pos.add(this.vel);
+    this.vel.add(this.acc);
+    this.vel.limit(this.topSpeed);
+  }
+
+  show() {
+    stroke(0);
+    fill(175);
+    circle(this.pos.x, this.pos.y, 48);
+  }
+
+  checkEdges() {
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+    } else if (this.pos.x < 0) {
+      this.pos.x = width;
+    }
+
+    if (this.pos.y > height) {
+      this.pos.y = 0;
+    } else if (this.pos.y < 0) {
+      this.pos.y = height;
+    }
+  }
+
+  // excersize 1.4
+  // limit(max){
+  //  if(max > 10){
+  //  vel.normalize();
+  //  vel.mult(max);
+  //  }
+  //}
+}
