48a2d45f54fc5fc38b0505de9435d5576d593d6e
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-08-02 17:30:31 +0200

    2.3 gravity scaled by mass

---
+++ codingtrain/TNOCbook/2_forces_TNOC/examples/2.3_gravity_scaled_by_mass/sketch.js
@@ -0,0 +1,33 @@
+let moverA;
+let moverB;
+
+function setup() {
+  createCanvas(640, 320);
+
+  moverA = new Mover(100, 30, 10);
+  moverB = new Mover(400, 30, 2);
+}
+
+function draw() {
+  background(255);
+
+  let gravity = createVector(0, 0.1);
+  let gravityA = p5.Vector.mult(gravity, moverA.mass);
+  moverA.applyForce(gravityA);
+  let gravityB = p5.Vector.mult(gravity, moverB.mass);
+  moverB.applyForce(gravityB);
+
+  if (mouseIsPressed) {
+    let wind = createVector(0.1, 0);
+    moverA.applyForce(wind);
+    moverB.applyForce(wind);
+  }
+
+  moverA.show();
+  moverA.update();
+  moverA.checkEdges();
+
+  moverB.show();
+  moverB.update();
+  moverB.checkEdges();
+}
