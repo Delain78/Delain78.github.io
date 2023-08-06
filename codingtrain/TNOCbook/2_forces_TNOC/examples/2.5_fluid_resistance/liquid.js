30cc8ab869ffdf09b2d02883b1d0eb287f23b6de
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-08-06 15:29:38 +0200

    uploaded 2.5 fluid resistance

---
+++ codingtrain/TNOCbook/2_forces_TNOC/examples/2.5_fluid_resistance/liquid.js
@@ -0,0 +1,40 @@
+class Liquid {
+  constructor(x, y, w, h, c) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.c = c;
+  }
+
+  contains(mover) {
+    // store the position in a different variable to make code more readable
+    let pos = mover.pos;
+
+    // This Boolean expression determines if the position vector is contained         within the rectangle defined by the Liquid class
+    return (
+      pos.x > this.x &&
+      pos.x < this.x + this.w &&
+      pos.y > this.y &&
+      pos.y < this.y + this.h
+    );
+  }
+
+  drag(mover) {
+    let speed = mover.vel.mag();
+    let dragMagnitude = this.c * speed * speed; // magnitude formula
+
+    let dragForce = mover.vel.copy();
+    dragForce.mult(-1); //direction formula
+
+    dragForce.setMag(dragMagnitude); //magnitude and direction together.
+
+    return dragForce;
+  }
+
+  show() {
+    noStroke();
+    fill(200);
+    rect(this.x, this.y, this.w, this.h);
+  }
+}
---
+++ codingtrain/TNOCbook/2_forces_TNOC/examples/2.5_fluid_resistance/mover.js
@@ -0,0 +1,64 @@
+class Mover {
+  constructor(x, y, mass) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.mass = mass;
+    this.r = this.mass * 8; //diameter is this.mass * 16, dus r is
+  }
+
+  applyForce(force) {
+    let f = p5.Vector.div(force, this.mass);
+    this.acc.add(f);
+  }
+
+  contactEdge() {
+    return this.pos.y > height - this.r - 1;
+  }
+
+  frictionF(c) {
+    if (this.contactEdge()) {
+      let normal1 = this.mass;
+      let frictionMag = c * normal1;
+
+      let friction = this.vel.copy();
+      friction.normalize();
+      friction.mult(-1);
+
+      friction.mult(frictionMag);
+
+      this.applyForce(friction);
+    }
+  }
+  
+ 
+
+  checkEdges() {
+    let bounce = 0.9;
+
+    if (this.pos.x >= width - this.r) {
+      this.pos.x = width - this.r;
+      this.vel.x *= -bounce;
+    } else if (this.pos.x <= this.r) {
+      this.pos.x = this.r;
+      this.vel.x *= -bounce;
+    }
+
+    if (this.pos.y > height - this.r - 1) {
+      this.pos.y = height - this.r;
+      this.vel.y *= -bounce;
+    }
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  show() {
+    stroke(0);
+    fill(175);
+    circle(this.pos.x, this.pos.y, this.r * 2);
+  }
+}
