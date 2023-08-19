80035abb33300d11549ef84a7f222fa5a96f2184
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-08-19 15:41:48 +0200

    uploaded 3.5 simple harmonic motion

---
+++ codingtrain/TNOCbook/3_Oscillation_TNOC/example/3.5_Simple_harmonic_motion/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(640, 240);
+}
+
+function draw() {
+  background(255);
+  
+  let amplitude = 100;   //pixels
+  let period = 120;      //framecount for tracking time
+  
+  let x = amplitude * sin(TWO_PI * frameCount/ period);
+  
+  stroke(0);
+  strokeWeight(2);
+  fill(127);
+  translate(width/2, height/2);
+  line(0,0,x,0);
+  circle(x,0,48)
+}
