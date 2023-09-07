f53868a3a678fdc5778fc79f209ecb53ae5778ea
Delain78 <135148655+Delain78@users.noreply.github.com>
2023-09-07 15:39:59 +0200

    uploaded ex 4.1 run method

---
+++ codingtrain/TNOCbook/4_particlesystem_TNOC/exercise/ex_4.1_run_method/sketch.js
@@ -0,0 +1,21 @@
+let particle;
+
+function setup() {
+  createCanvas(640, 360);
+  particle = new Particle(width / 2, 20);
+}
+
+function draw() {
+  background(255);
+  particle.run();
+  //particle.update();
+  //particle.show();
+  
+  //let gravity = createVector (0, 0.1);
+  //particle.applyForce(gravity);
+  
+  if (particle.isDead()){
+    particle = new Particle (width/2, 20);
+    console.log("Particle dead!")
+  }
+}
